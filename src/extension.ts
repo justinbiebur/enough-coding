02b4db77accd3aa155e057065a2e664b9fb1d9b4
debjyoti2019 <db.debjyotibanerjee@gmail.com>
2020-05-25 01:14:07 +0530

    set duration type

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const duration=vscode.workspace.getConfiguration('enough-coding').get('duration')
+	const duration=<number>vscode.workspace.getConfiguration('enough-coding').get('duration')
 	console.log(duration)
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -15,14 +15,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('enough-coding.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	setTimeout(()=>{
 
-	context.subscriptions.push(disposable);
+	},duration*100)
 }
 
 // this method is called when your extension is deactivated
