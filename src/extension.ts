d5cfd35797214847873893b07ac8c14d3da78969
debjyoti2019 <db.debjyotibanerjee@gmail.com>
2020-05-24 06:16:31 +0530

    init

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from enough coding!');
+		vscode.window.showInformationMessage('Hello World!');
 	});
 
 	context.subscriptions.push(disposable);
